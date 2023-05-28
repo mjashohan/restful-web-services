f9e825900c97b58960cd7647dc817e30516c1dd4
Muhammad Shohan <muhammad.shohan@ancud.de>
2023-05-28 20:25:45 +0200

    Made custom response entity response handler

---
+++ src/main/java/com/shohan/rest/webservices/restfulwebservices/exception/CutomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.shohan.rest.webservices.restfulwebservices.exception;
+
+import com.shohan.rest.webservices.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class CutomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
