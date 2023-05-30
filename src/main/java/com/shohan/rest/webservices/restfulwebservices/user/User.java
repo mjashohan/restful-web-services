47267972483a9a0fe636cf92b94903c5d543345a
muhammad_shohan <muhammad.shohan@ancud.de>
2023-05-30 23:14:58 +0200

    Added data filtering examples

--- src/main/java/com/shohan/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/shohan/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.shohan.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -8,8 +9,10 @@ import java.time.LocalDate;
 public class User {
     private Integer id;
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @JsonProperty("user_name")
     private String name;
     @Past(message = "Birth date should be in the past")
+    @JsonProperty("birth_date")
     private LocalDate birthdate;
 
     public User(Integer id, String name, LocalDate birthdate) {
